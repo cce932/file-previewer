67a5450e8fd44540e20826387598f3e694f14f18
unknown <dena.chen@shopback.com>
2021-05-21 23:27:20 +0800

    add filePreviewer of Ajax scenario 1

---
+++ FilePreviewer/Scripts/filePreviewer.js
@@ -0,0 +1,10 @@
+﻿window.onload = function () {
+    filePreviewerRegister();
+}
+
+function filePreviewerRegister() {
+    $("#image-data").fileinput({
+        uploadUrl: "http://localhost:9090/image", // need an enapoint for uploading files
+        maxFileCount: 5
+    });
+}
